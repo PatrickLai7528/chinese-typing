2a73c1bd1ce2782c596a669f7de3c1e79dace170
Patrick Lai <weitanai@pinduoduo.com>
2019-11-14 17:34:23 +0800

    docs(*): move docs outside

---
+++ precache-manifest.0d12ad5547ad47a0021f6b1f523ffe67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "b4ad61d3ad8aefc3d438f35d2300339e",
+    url: "/index.html"
+  },
+  {
+    revision: "03317d10c8d423b8cb99",
+    url: "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    revision: "d55d8352d85042a923d6",
+    url: "/static/css/main.fea08177.chunk.css"
+  },
+  {
+    revision: "03317d10c8d423b8cb99",
+    url: "/static/js/2.2d765830.chunk.js"
+  },
+  {
+    revision: "d55d8352d85042a923d6",
+    url: "/static/js/main.e07d970e.chunk.js"
+  },
+  {
+    revision: "7a7e91044952ee4f946f",
+    url: "/static/js/runtime-main.2b0054c7.js"
+  }
+]);
